55c114e2fc4b1a61f7d9424004bd668909b96d91
Alex Arriaga <alexzander.arriaga@base22.com>
2018-10-31 15:33:37 -0600

    Agregar ejemplo de componente "Header"

---
+++ components/common/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-container>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <b-navbar-brand href="#">
+          <i class="fas fa-film"></i>
+          <span>Movify</span>
+        </b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-nav-item-dropdown right>
+              <!-- Using button-content slot -->
+              <template slot="button-content">
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Signout</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+
+  </header>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        name: 'Movify',
+      };
+    },
+  };
+</script>
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        curso-profesional-nuxt-js
-      </h1>
-      <h2 class="subtitle">
-        Movify
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+
     </div>
   </section>
 </template>
@@ -33,15 +17,6 @@ export default {
 </script>
 
 <style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
