8362356694993c8e8a0a02230f9683c4e4640556
Alex Arriaga <alexzander.arriaga@base22.com>
2018-11-06 18:01:44 -0600

    Mejorar página de inicio

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
   <section class="container">
-    <div>
-      Index
+    <div class="col-12">
+      <section class="b-section">
+        <div class="b-section__image text-center mt-5">
+          <img src="~/assets/img/index/cinema.png" alt="Let's find an amazing movie" class="col-12 col-sm-8 col-md-6">
+        </div>
+
+        <h1 class="b-section__title text-center mt-4">Movify!</h1>
+        <h2 class="b-section__subtitle text-center mt-4">Movies for everyone for good</h2>
+        <div class="text-center mt-3">
+          <nuxt-link to="/search" class="btn btn-warning btn-lg">Search</nuxt-link>
+        </div>
+      </section>
     </div>
   </section>
 </template>
